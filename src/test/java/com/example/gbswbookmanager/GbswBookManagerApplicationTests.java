44c1df537fd4a22e127034e083913eb63365eb1e
eoguddl <nicenicnic123@gmail.com>
2022-08-21 19:32:01 +0900

    first commit

--- src/test/java/com/example/gbswbookmanager/GbswBookManagerApplicationTests.java
+++ src/test/java/com/example/gbswbookmanager/GbswBookManagerApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.gbswbookmanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//hi
 @SpringBootTest
 class GbswBookManagerApplicationTests {
 
