3157eb3694f54cf7a096dbde7f35b0a2d895650b
eoguddl <nicenicnic123@gmail.com>
2022-09-01 15:50:54 +0900

    last commit(22)

--- src/main/java/com/example/gbswbookmanager/GbswBookManagerApplication.java
+++ src/main/java/com/example/gbswbookmanager/GbswBookManagerApplication.java
@@ -31,24 +31,24 @@ public class GbswBookManagerApplication {
     @Bean
     CommandLineRunner run(UserService userService, BookService bookService) {
         return args -> {
-            userService.saveUserTest(new User(null, "박대일", "2101", "nicenicnic1@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
-            userService.saveUserTest(new User(null, "박대이", "2102", "nicenicnic12@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
-            userService.saveUserTest(new User(null, "박대삼", "2103", "nicenicnic123@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
+//            userService.saveUserTest(new User(null, "박대일", "2101", "nicenicnic1@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
+//            userService.saveUserTest(new User(null, "박대이", "2102", "nicenicnic12@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
+            userService.saveUserTest(new User(null, "박대형", "2107", "eoguddl.dev@gmail.com", "1234", new ArrayList<>(), new ArrayList<>()));
             userService.saveUserTest(new User(null, "김진효", "2206", "admin@jinhyo.dev", "1234", new ArrayList<>(), new ArrayList<>()));
 
             userService.saveRole(new Role(null, "ROLE_USER"));
             userService.saveRole(new Role(null, "ROLE_ADMIN"));
 
-            bookService.addBook(new Book(null, "이펙티브 타입스크립트", "대형이", "대형출판사", 5, 4));
-            bookService.addBook(new Book(null, "이펙티브 자바", "대형대형이", "대형출판사", 2, 1));
-            bookService.addBook(new Book(null, "파이썬 기초", "지뇨", "지뇨출판사", 1, 1));
-            bookService.addBook(new Book(null, "파이썬 고급", "지뇨지뇨", "지뇨출판사", 3, 2));
-            bookService.addBook(new Book(null, "공룡 중급", "지뇨지뇨", "지뇨출판사", 10, 2));
+//            bookService.addBook(new Book(null, "이펙티브 자바", "대형대형이", "대형출판사", 2, 1));
+//            bookService.addBook(new Book(null, "파이썬 기초", "지뇨", "지뇨출판사", 1, 1));
+//            bookService.addBook(new Book(null, "파이썬 고급", "지뇨지뇨", "지뇨출판사", 3, 2));
+//            bookService.addBook(new Book(null, "공룡 중급", "지뇨지뇨", "지뇨출판사", 10, 2));
+//            bookService.addBook(new Book(null, "이펙티브 타입스크립트", "대형이", "대형출판사", 5, 4));
 
-            userService.addRoleToUser("nicenicnic1@gmail.com", "ROLE_USER");
-            userService.addRoleToUser("nicenicnic12@gmail.com", "ROLE_USER");
-            userService.addRoleToUser("nicenicnic123@gmail.com", "ROLE_USER");
-            userService.addRoleToUser("nicenicnic123@gmail.com", "ROLE_ADMIN");
+//            userService.addRoleToUser("nicenicnic1@gmail.com", "ROLE_USER");
+//            userService.addRoleToUser("nicenicnic12@gmail.com", "ROLE_USER");
+            userService.addRoleToUser("eoguddl.dev@gmail.com", "ROLE_ADMIN");
+            userService.addRoleToUser("admin@jinhyo.dev", "ROLE_ADMIN");
 
 //            userService.addBookToUser("nicenicnic1@gmail.com", "이펙티브 타입스크립트");
 //            userService.addBookToUser("nicenicnic1@gmail.com", "이펙티브 자바");
