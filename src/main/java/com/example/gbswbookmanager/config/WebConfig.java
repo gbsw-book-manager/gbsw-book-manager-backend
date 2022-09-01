a09e4e56c93cc0e287edb38352539f547e6430e7
eoguddl <nicenicnic123@gmail.com>
2022-09-01 20:18:39 +0900

    last commit(23)

--- src/main/java/com/example/gbswbookmanager/config/WebConfig.java
+++ src/main/java/com/example/gbswbookmanager/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
